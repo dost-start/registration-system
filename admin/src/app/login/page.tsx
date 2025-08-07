4e393c3a85f7e3de02b3070bb398a117dcb16ddb
Septchi <greenenji@gmail.com>
2025-08-07 11:38:04 +0800

    login system

--- admin/src/app/login/page.tsx
+++ admin/src/app/login/page.tsx
@@ -1,11 +1,43 @@
+'use client';
+
 import Image from "next/image";
-import { orbitron } from "~/lib/fonts";
+import { orbitron } from "@/lib/fonts";
 import { createClient } from "@supabase/supabase-js";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
+
 
 export default function Login() {
   const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!
   const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+  const router = useRouter();
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+      })
+      if (error) {
+        throw new Error(error.message);
+      }
+      router.push('/');
+
+    }
+    catch (error: any) {
+      console.error("Supabase ERROR: ", error.message);
+    }
+  }
+
+
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+
   return (
     <div className="flex flex-col items-center justify-center max-h-dvh h-dvh px-10">
       <div
@@ -20,13 +52,13 @@ export default function Login() {
             className="mb-12 drop-shadow-[0_4px_8px_rgba(0,0,0,0.15)]"
           />
         </div>
-        <form className="flex flex-col space-y-8" >
+        <form className="flex flex-col space-y-8" onSubmit={handleSubmit}>
           <div className="flex flex-col space-y-4">
-            <label>Username</label>
-            <input type="text" placeholder="Enter your username" />
+            <label>Email</label>
+            <input type="email" value={email} placeholder="Enter your email" onChange={(e) => setEmail(e.target.value)} />
 
             <label>Password</label>
-            <input type="password" placeholder="Password" />
+            <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
           </div>
           <button className="w-1/2 bg-primary rounded-md" type="submit"> Submit </button>
         </form>
